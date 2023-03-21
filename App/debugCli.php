f7745b3883a94ca10503de1407d4d969a60b73e7
xenofon pantsos <pantsosxen23@gmail.com>
2023-03-21 10:46:12 +0200

    add js and first page

--- App/debugCli.php
+++ App/debugCli.php
@@ -2,12 +2,16 @@
 require_once "../vendor/autoload.php";
 use App\Controllers\AuthenticationController;
 use App\Services\User\UserDetails;
+use App\View\Page;
 
-$data=['userName'=>'johndoe','password'=>'mysecretpassword'];
-$controller=new AuthenticationController($data);
-echo  $controller->enableLogin();
+//$data=['userName'=>'johndoe','password'=>'mysecretpassword'];
+//$controller=new AuthenticationController($data);
+//echo  $controller->enableLogin();
 //$userDetails = UserDetails::getInstance(1);
 //$test=$userDetails->getUserId();
 //$data=['userName'=>'johndoe','password'=>'mysecretpassword'];
 //$controller=new AuthenticationController($data);
 // echo $controller->test();
+$page=new Page();
+$test=$page->generatePage('/index.html');
+echo 'malakia';
