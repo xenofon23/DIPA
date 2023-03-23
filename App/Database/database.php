f4ba1a23fca3c18be95586346a2537932367e3b4
Andreasmeis <78660594+Andreasmeis@users.noreply.github.com>
2023-03-23 13:11:56 +0200

    .

--- App/Database/database.php
+++ App/Database/database.php
@@ -9,25 +9,23 @@ trait database
 {
     public function mongo($mongoCollection): Collection
     {
-        $username='superadmin';
-        $password='password';
-        $host='192.168.242.129';
-        $port='27017';
-        $database='admin';
+//        $username='superadmin';
+//        $password='password';
+//        $host='192.168.242.129';
+//        $port='27017';
+//        $database='admin';
+//
+//        $uri = sprintf(
+//            'mongodb://%s:%s@%s:%s/%s',
+//            $username,
+//            $password,
+//            $host,
+//            $port,
+//            $database
+//        );
 
-        $uri = sprintf(
-            'mongodb://%s:%s@%s:%s/%s',
-            $username,
-            $password,
-            $host,
-            $port,
-            $database
-        );
-
-        $mongoConnection=new Client($uri);
-//        $mongoConnection = new Mongo('mongodb://' .
-//            $_SERVER['APP_DB_ADDRESS'] . ':' .
-//            $_SERVER['APP_DB_PORT']);
+//        $mongoConnection=new Client($uri);
+        $mongoConnection = new Mongo('mongodb://localhost:27017/');
         $db = $mongoConnection->selectDatabase('dipa');
         return $db->selectCollection($mongoCollection);
     }
